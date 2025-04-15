169f15fb446dbe845d64c1c812b38f292af965f7
guilherme-alc <guilhermealc01@gmail.com>
2025-04-15 12:05:34 -0300

    feat: adiciona classe Template

---
+++ MensagensNPS/Models/Template.cs
@@ -0,0 +1,17 @@
+﻿namespace MensagensNPS.Models
+{
+    public class Template
+    {
+        public string Nome { get; set; }
+        public string Conteudo { get; set; }
+        public DateTime? DataCadastro { get; set; }
+        public DateTime? DataExclusao { get; set; }
+
+        public Template(string nome, string conteudo)
+        {
+            Nome = nome;
+            Conteudo = conteudo;
+            DataCadastro = DateTime.Now;
+        }
+    }
+}
